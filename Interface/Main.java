5a1bfd7f89cbfa491ae7f69e3202036c04700d9e
vladilima <50593990+vladilima@users.noreply.github.com>
2024-05-27 20:08:18 -0300

    Add files via upload

--- Interface/Main.java
+++ Interface/Main.java
@@ -2,7 +2,13 @@ import br.univille.log.Level;
 import br.univille.log.LoggerFactory;
 
 public class Main {
-    public void main(String[] args) {
-        LoggerFactory.onConsole().log(Level.DEBUG, "AAA");
+    public static void main(String[] args) {
+        LoggerFactory.onConsole().log(Level.DEBUG, "Debug");
+        LoggerFactory.onConsole().log(Level.WARNING, "Aviso");
+        LoggerFactory.onConsole().log(Level.ERROR, "Erro");
+
+        LoggerFactory.onFile().log(Level.DEBUG, "Debug");
+        LoggerFactory.onFile().log(Level.WARNING, "Aviso");
+        LoggerFactory.onFile().log(Level.ERROR, "Erro");
     }
 }
--- Interface/br/univille/log/LoggerConsole.java
+++ Interface/br/univille/log/LoggerConsole.java
@@ -1,8 +1,24 @@
 package br.univille.log;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class LoggerConsole implements Logger {
+    String color;
     @Override
     public void log(Level level, String message) {
-        System.out.println("CONSOLE");
+        switch (level) {
+            case DEBUG:
+                color = "\u001B[32m";
+                break;
+            case WARNING:
+                color = "\u001B[33m";
+                break;
+            case ERROR:
+                color = "\u001B[31m";
+                break;
+        }
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
+        System.out.println(color + "[" + timestamp + "]: " + level + " - " + message + color);
     }
 }
